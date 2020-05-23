900a97ae93588230bc2b197707d4fe2f234a3951
Phil Oterdahl <tintin.oterdahl@hotmail.com>
2020-05-23 11:31:03 -0700

    Update InstallationScript.ps1

--- InstallationScript.ps1
+++ InstallationScript.ps1
@@ -1,6 +1,7 @@
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+choco install googlechrome
 choco install nodejs
 choco install powertoys
 choco install visualstudio2019community
